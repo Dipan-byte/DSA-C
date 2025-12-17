a223ff9f89a7a6e4c2ef420f5d6b2e9399bfaaee
Dipan Mallick <dipanmallick39@gmail.com>
2025-12-17 21:51:23 +0530

    done

---
+++ array/findleaderele.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int maxFromRight = arr[n - 1];
+    printf("%d ", maxFromRight);
+
+    for (int i = n - 2; i >= 0; i--) {
+        if (arr[i] > maxFromRight) {
+            maxFromRight = arr[i];
+            printf("%d ", arr[i]);
+        }
+    }
+
+    return 0;
+}
